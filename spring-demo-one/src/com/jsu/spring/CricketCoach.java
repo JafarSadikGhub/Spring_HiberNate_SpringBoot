5daf52e8141c1bf270dab2ce23957f81c8fa933d
jafar95 <jafar.sadik@northsouth.edu>
2021-01-30 19:06:33 +0600

    initial commit 0.5

--- spring-demo-one/src/com/jsu/spring/CricketCoach.java
+++ spring-demo-one/src/com/jsu/spring/CricketCoach.java
@@ -1,27 +1,42 @@
 package com.jsu.spring;
 
-public class CricketCoach implements Coach 
-{
+public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
-	
-	public CricketCoach()
-	{
+	private String emailAddress;
+	private String team;
+
+	public CricketCoach() {
 		System.out.println("CrciketCoach: inside no arg constructor.");
 	}
-	
-	
-	public FortuneService getFortuneService() { 
-		return fortuneService;
+
+	public String getEmailAddress() {
+		return emailAddress;
 	}
 
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CrciketCoach: inside setEmailAddress Setter.");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CrciketCoach: inside setTeam Setter.");
+		this.team = team;
+	}
+
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
 
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CrciketCoach: inside setFortuneService Setter.");
 		this.fortuneService = fortuneService;
-		
-	}
 
+	}
 
 	@Override
 	public String getDailyWorkout() {
--- spring-demo-one/src/com/jsu/spring/SetterDemoApp.java
+++ spring-demo-one/src/com/jsu/spring/SetterDemoApp.java
@@ -4,18 +4,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SetterDemoApp {
 
-	public static void main(String[] args) 
-	{
-	   //load the spring config file
-	   ClassPathXmlApplicationContext context = 
-			     new ClassPathXmlApplicationContext("applicationContext.xml");	
-		
-	  // retrieve bean from spring container
+	public static void main(String[] args) {
+		// load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve bean from spring container
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
-      // call methods on the bean
-     System.out.println(theCoach.getDailyWorkout());
-     System.out.println(theCoach.getDailyFortune());
-	 // close the context
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+
+		// close the context
 		context.close();
 
 	}
