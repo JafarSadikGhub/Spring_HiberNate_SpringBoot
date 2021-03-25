4f2fbb41b6278ec107ecb9ab110552e9a6533fa1
jafar95 <jafar.sadik@northsouth.edu>
2021-03-25 15:36:00 +0600

    l1

--- spring-demo-one/src/com/jsu/spring/BeanLifeCycleDemoApp.java
+++ spring-demo-one/src/com/jsu/spring/BeanLifeCycleDemoApp.java
@@ -7,16 +7,17 @@ public class BeanLifeCycleDemoApp {
 	public static void main(String[] args) {
 		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"beansScope-applicationContext.xml");
+				"beanLifeCycle-applicationContext.xml");
 		// retrieve bean from Spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach alphaCoach = context.getBean("myCoach", Coach.class);
-		System.out.println(theCoach.getDailyFortune());
-		boolean value = (theCoach == alphaCoach);
-		String str = String.valueOf(value);
-		System.out.println("Are theCoach and alphaCoach equal?" + str);
-		System.out.println(theCoach);
-		System.out.println(alphaCoach);
+		// Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		System.out.println(theCoach.getDailyWorkout());
+		// boolean value = (theCoach == alphaCoach);
+		// String str = String.valueOf(value);
+		// System.out.println("Are theCoach and alphaCoach equal?" + str);
+		// System.out.println(theCoach);
+		// System.out.println(alphaCoach);
+		context.close();
 	}
 
 }
